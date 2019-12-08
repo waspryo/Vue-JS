57484fd79923b3af388f1a0fbc44346b2d17215f
waspryo <wasp2123321@gmail.com>
2019-12-08 14:20:37 +0900

    v-model and v-on

--- main.js
+++ main.js
@@ -1,21 +1,22 @@
 const app = new Vue({
   el: "#app",
   data: {
-    friends: ["john", "sala"],
-    john: {
-      first: "john",
-      last: "conner",
-      age: 25
-    },
-    sala: {
-      first: "sala",
-      last: "conner",
-      age: 55
-    }
+    friends: [
+      {
+        first: "john",
+        last: "conner",
+        age: 25
+      },
+      {
+        first: "sala",
+        last: "conner",
+        age: 55
+      }
+    ]
   },
   filters: {
     ageInOneYear(age) {
-        return age + 1
+      return age + 1;
     },
     fullName(value) {
       return `${value.first} ${value.last}`;
@@ -23,7 +24,14 @@ const app = new Vue({
   },
   template: `
     <div>
-        <h2 v-for="friend in friends">{{ friend }}</h2>
+        <h2 v-for="friend in friends">
+        <h4>{{ friend | fullName }}</h4>
+        <h5>age: {{friend.age}}</h5>
+        <button v-on:click="friend.age = friend.age + 1">+</button>
+        <button v-on:click="friend.age = friend.age - 1">-</button>
+        <input v-model="friend.first"/>
+        <input v-model="friend.last"/>
+        </h2>
     </div>
     `
 });
