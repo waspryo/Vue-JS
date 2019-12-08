59471736eb0429d7fdcdf319b6a7ed1ae49d841f
waspryo <wasp2123321@gmail.com>
2019-12-08 14:23:43 +0900

    using methods

--- main.js
+++ main.js
@@ -22,13 +22,21 @@ const app = new Vue({
       return `${value.first} ${value.last}`;
     }
   },
+  methods: {
+    incrementAge(friend) {
+        friend.age = friend.age + 1
+    },
+    decrementAge(friend) {
+        friend.age = friend.age - 1
+    }
+  },
   template: `
     <div>
         <h2 v-for="friend in friends">
         <h4>{{ friend | fullName }}</h4>
         <h5>age: {{friend.age}}</h5>
-        <button v-on:click="friend.age = friend.age + 1">+</button>
-        <button v-on:click="friend.age = friend.age - 1">-</button>
+        <button v-on:click="incrementAge(friend)">+</button>
+        <button v-on:click="decrementAge(friend)">-</button>
         <input v-model="friend.first"/>
         <input v-model="friend.last"/>
         </h2>
