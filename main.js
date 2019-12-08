e11488d1b3c931ba79692b16e725283d516b72bc
waspryo <wasp2123321@gmail.com>
2019-12-08 13:42:04 +0900

    filterFunction

--- main.js
+++ main.js
@@ -10,26 +10,34 @@ const app = new Vue({
       first: "sala",
       last: "conner",
       age: 55
-    },
+    }
   },
   computed: {
-    johnFullName() {
-      return `${this.john.first} ${this.john.last}`;
+    // johnFullName() {
+    //   return `${this.john.first} ${this.john.last}`;
+    // },
+    // salaFullName(){
+    //   return `${this.sala.first} ${this.sala.last}`
+    // },
+    johnAgeInOneYear() {
+      return this.john.age + 1;
+    }
+  },
+  filters: {
+    ageInOneYear(age) {
+        return age + 1
     },
-    salaFullName(){
-      return `${this.sala.first} ${this.sala.last}`
-    },
-    johnAgeInOneYear(){
-      return this.john.age + 1
+    fullName(value) {
+      return `${value.first} ${value.last}`;
     }
   },
   template: `
     <div>
-        <h1>Name: {{ johnFullName }}</h1>
-        <h2>Age: {{ johnAgeInOneYear }}</h2>
+        <h1>Name: {{ john | fullName }}</h1>
+        <h2>Age: {{ john.age | ageInOneYear }}</h2>
         <br>
-        <h1>Name: {{ salaFullName }}</h1>
-        <h2>Age: {{ sala.age }}</h2>
+        <h1>Name: {{ sala | fullName }}</h1>
+        <h2>Age: {{ sala.age | ageInOneYear }}</h2>
     </div>
     `
 });
