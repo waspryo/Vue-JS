81123df7a475c11ac670a05c1814b5bcd1aad351
waspryo <wasp2123321@gmail.com>
2019-12-08 13:35:54 +0900

    computed

--- main.js
+++ main.js
@@ -2,18 +2,34 @@ const app = new Vue({
   el: "#app",
   data: {
     john: {
-      name: "body",
+      first: "john",
+      last: "conner",
       age: 25
     },
-    aim: {
-      name: "deli",
-      age: 25
+    sala: {
+      first: "sala",
+      last: "conner",
+      age: 55
+    },
+  },
+  computed: {
+    johnFullName() {
+      return `${this.john.first} ${this.john.last}`;
+    },
+    salaFullName(){
+      return `${this.sala.first} ${this.sala.last}`
+    },
+    johnAgeInOneYear(){
+      return this.john.age + 1
     }
   },
   template: `
     <div>
-        <h1>Name: {{ aim.name }}</h1>
-        <h2>Age: {{ aim.age }}</h2>
+        <h1>Name: {{ johnFullName }}</h1>
+        <h2>Age: {{ johnAgeInOneYear }}</h2>
+        <br>
+        <h1>Name: {{ salaFullName }}</h1>
+        <h2>Age: {{ sala.age }}</h2>
     </div>
     `
 });
