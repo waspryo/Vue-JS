5552adc2977cca1b86dae32e394a8e93986bc158
waspryo <wasp2123321@gmail.com>
2019-12-09 22:30:19 +0900

    component

--- main.js
+++ main.js
@@ -1,3 +1,34 @@
+Vue.component("friend-component", {
+  props: ['friend'],
+  filters: {
+    ageInOneYear(age) {
+      return age + 1;
+    },
+    fullName(value) {
+      return `${value.first} ${value.last}`;
+    }
+  },
+  methods: {
+    incrementAge(friend) {
+      friend.age = friend.age + 1;
+    },
+    decrementAge(friend) {
+      friend.age = friend.age - 1;
+    }
+  },
+  template: `
+  <div>
+    <h4>{{ friend | fullName }}</h4>
+    <h5>age: {{friend.age}}</h5>
+    <button v-on:click="incrementAge(friend)">+</button>
+    <button v-on:click="decrementAge(friend)">-</button>
+    <input v-model="friend.first"/>
+    <input v-model="friend.last"/>
+    </h2>
+  </div>  
+  `
+});
+
 const app = new Vue({
   el: "#app",
   data: {
@@ -14,32 +45,9 @@ const app = new Vue({
       }
     ]
   },
-  filters: {
-    ageInOneYear(age) {
-      return age + 1;
-    },
-    fullName(value) {
-      return `${value.first} ${value.last}`;
-    }
-  },
-  methods: {
-    incrementAge(friend) {
-        friend.age = friend.age + 1
-    },
-    decrementAge(friend) {
-        friend.age = friend.age - 1
-    }
-  },
   template: `
     <div>
-        <h2 v-for="friend in friends">
-        <h4>{{ friend | fullName }}</h4>
-        <h5>age: {{friend.age}}</h5>
-        <button v-on:click="incrementAge(friend)">+</button>
-        <button v-on:click="decrementAge(friend)">-</button>
-        <input v-model="friend.first"/>
-        <input v-model="friend.last"/>
-        </h2>
+        <friend-component v-for="item in friends" v-bind:friend="item"/>
     </div>
     `
 });
